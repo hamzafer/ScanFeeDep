de39a8505c45c61d9be1f3d352f1992874281938
Hamza <hamzafer3@gmail.com>
2022-07-05 16:29:53 +0500

    Updates

---
+++ precache-manifest.70a1ba9dba7c40a8fd23892840fc9249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2ab80e9b4085dfd0935ffd0a57c316",
+    "url": "/ScanFeeDep/index.html"
+  },
+  {
+    "revision": "5bb8158bde29d84059ad",
+    "url": "/ScanFeeDep/static/js/2.b12b5809.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ScanFeeDep/static/js/2.b12b5809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05c27dc5def29cf1468",
+    "url": "/ScanFeeDep/static/js/main.c5c18f5a.chunk.js"
+  },
+  {
+    "revision": "29d9beb438e5722d95dc",
+    "url": "/ScanFeeDep/static/js/runtime-main.ffe46b44.js"
+  }
+]);
